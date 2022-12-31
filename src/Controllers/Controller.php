b99a3c380841b0a1566790ac108d80d81a23fd72
silakchhum <chhumsilak@gmail.com>
2022-12-31 09:36:50 +0700

    feat: :sparkles: setup core controller

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -4,8 +4,11 @@ namespace Phumsoft\Phumpie\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
 use Phumsoft\Phumpie\Traits\Restable;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class Controller extends BaseController
 {
-    use Restable;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, Restable;
 }
